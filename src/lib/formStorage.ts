a68fe44c011ff61d19ae8aa7170dac1bafc07854
generic-name-2166 <kirbesson277@mail.ru>
2023-11-13 19:03:55 +0300

    Storage functions

--- src/lib/formStorage.ts
+++ src/lib/formStorage.ts
@@ -9,3 +9,69 @@ export interface FormData {
 
 const temp: FormData[] = [];
 export const formData = writable(temp);
+
+function storageAvailable(type: string) {
+  let storage;
+  try {
+    // I don't even know what it wants from me here
+    //@ts-expect-error
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      (e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+export function readFromLocalStorage(): FormData[] | null {
+  if (!storageAvailable("localStorage")) {
+    console.error("localStorage no available");
+    return null;
+  } 
+  
+  const item: string | null = localStorage.getItem("formData");
+
+  if (!item) {
+    return null;
+  }
+
+  return JSON.parse(item);
+}
+
+function setToLocalStorage(data: (FormData | null)[]): void {
+  try {
+    localStorage.setItem("formData", JSON.stringify(data))
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function saveCellToLocalStorage(cellData: FormData, cellNumber: number): void {
+  if (!storageAvailable("localStorage")) {
+    console.error("localStorage no available");
+    return;
+  }
+
+  const prevFormData: FormData[] | null = readFromLocalStorage();
+
+  if (!prevFormData) {
+    const mockList = new Array(cellNumber + 1);
+    mockList[cellNumber] = cellData;
+
+    setToLocalStorage(mockList);
+    return;
+  }
+
+  const formDataList: FormData[] = prevFormData!;
+  formDataList[cellNumber] = cellData;
+
+  setToLocalStorage(formDataList);
+}
