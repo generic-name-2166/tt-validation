c61de88fa2f7433e1ada827e2b6aaec78a5a8262
generic-name-2166 <kirbesson277@mail.ru>
2023-12-05 17:05:22 +0300

    items

--- src/lib/CellComponents/CellTitle.svelte
+++ src/lib/CellComponents/CellTitle.svelte
@@ -1,43 +1,40 @@
 <script lang="ts">
   import { titleData } from "$lib/formStorage";
 
-  let documentTitle: string;
-  let manager: string;
-
-  function saveChangeTitle(): void {
-    titleData.update((title_data) => ({
-      ...title_data,
-      documentTitle: documentTitle,
-    }));
+  interface TitleItem {
+    key: string;
+    display: string;
+    value: string;
   }
 
-  function saveChangeManager(): void {
-    titleData.update((title_data) => ({ ...title_data, manager: manager }));
+  let items: TitleItem[] = [
+    { key: "documentTitle", display: "documentTitle", value: "" },
+    { key: "managerPosition", display: "managerPosition", value: "" },
+    { key: "managerName", display: "managerName", value: "" },
+  ]
+
+  function saveChange(key: string, value: string): void {
+    titleData.update((title_data) => {
+      title_data.set(key, value);
+      return title_data;
+    });
   }
 </script>
 
 <div>
   <h2>Титульный лист</h2>
 
-  <label for="documentTitle"> Тема работы </label>
-  <br />
-  <input
-    id="documentTitle"
-    type="text"
-    bind:value={documentTitle}
-    on:change={saveChangeTitle}
-  />
-
-  <br />
-
-  <label for="manager"> Manager in charge </label>
-  <br />
-  <input
-    id="manager"
-    type="text"
-    bind:value={manager}
-    on:change={saveChangeManager}
-  />
+  {#each items as item}
+    <label for={item.key}>{item.display}</label>
+    <br />
+    <input
+      id={item.key}
+      type="text"
+      bind:value={item.value}
+      on:change={() => { saveChange(item.key, item.value); }}
+    />
+    <br />
+  {/each}
 </div>
 
 <style>
