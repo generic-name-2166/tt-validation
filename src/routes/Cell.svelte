463c4503078a41b7dcc641fe283a824ca061fcd1
Maaaaashka <151385071+Maaaaashka@users.noreply.github.com>
2023-12-06 16:43:14 +0300

    Update Cell.svelte

--- src/routes/Cell.svelte
+++ src/routes/Cell.svelte
@@ -122,19 +122,55 @@
 </div>
 
 <style>
-  .cell {
-    width: 100px;
-    height: 100px;
-    background-color: #f2f2f2;
-    border: 1px solid #ccc;
-    text-align: center;
-    line-height: 100px;
-    font-size: 16px;
-    color: #333;
-    transition: background-color 0.3s ease;
-  }
+<head>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+  margin: 0;
+  padding: 0;
+}
 
-  .cell:hover {
-    background-color: #e0e0e0;
-  }
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+form {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #0056b3;
+}
+</head>
 </style>
